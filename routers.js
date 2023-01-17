70ff63bb764f52cea9eaa06cdfc67fa8532a856f
DimasNuryadin <dimasnuryadinn@gmail.com>
2023-01-17 22:53:03 +0700

    integration mongodb method get

--- routers.js
+++ routers.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const connection = require('./connection');
+
+const db = connection.db("db_latihan");
+const isConnected = { connected: false };
 
 router.get('/', (req, res) => {
   console.log('Hello Word')
@@ -7,5 +11,18 @@ router.get('/', (req, res) => {
     message: 'GET berhasil'
   })
 })
+
+router.get("/users", async (req, res) => {
+  try {
+    if ((isConnected.connected = true)) {
+      const users = await db.collection("users").find().toArray();
+      res.send({ data: users });
+    } else {
+      res.send({ message: "Koneksi database gagal" });
+    }
+  } catch (err) {
+    res.send({ message: err.message || "Internal server error" });
+  }
+});
 
 module.exports = router;
